6f5debcce84cf2267dd5f42832e6c182440534ab
BrianErikson <briankerikson@gmail.com>
2020-09-15 09:37:38 -0600

    get pid init

--- include/WindowManager.h
+++ include/WindowManager.h
@@ -2,17 +2,32 @@
 #include <vector>
 #include <i3ipc++/ipc.hpp>
 #include <i3/ipc.h>
+#include <X11/Xlib.h>
+
+struct WindowContent {
+  const pid_t pid;
+  const std::list<WindowContent> children;
+  const std::shared_ptr<i3ipc::container_t> i3node;
+};
 
 struct DisplayContent {
   const std::string display_name;
-  const std::list<std::shared_ptr<i3ipc::container_t>> content;
+  const std::list<WindowContent> windows;
 };
 
 class WindowManager {
  public:
   void print_tree();
  private:
+
   std::vector<DisplayContent> get_display_contents();
   void print_nodes(const std::shared_ptr<i3ipc::container_t>& root, int cur_depth);
+  WindowContent get_window(const std::shared_ptr<i3ipc::container_t>& container);
+  pid_t get_pid(const std::shared_ptr<i3ipc::container_t>& container);
+
+
+  _XDisplay *x_display{nullptr};
+  Screen *x_screen{nullptr};
+  Atom x_pid_atom{0};
   i3ipc::connection conn;
 };
--- src/WindowManager.cc
+++ src/WindowManager.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <i3ipc++/ipc.hpp>
 #include <WindowManager.h>
+#include <assert.h>
 
 std::string layout_to_string(i3ipc::ContainerLayout layout) {
   switch (layout) {
@@ -12,18 +13,22 @@ std::string layout_to_string(i3ipc::ContainerLayout layout) {
     case i3ipc::ContainerLayout::DOCKAREA: return "DOCKAREA";
     case i3ipc::ContainerLayout::OUTPUT: return "OUTPUT";
   }
+
+  return "";
 }
 
 void WindowManager::print_tree() {
-  for (const auto& display : this->get_display_contents()) {
-    std::cout << "Display: " << display.display_name << std::endl;
-    for (const auto& node : display.content) {
-      print_nodes(node, 1);
+  for (const auto &display_container : this->get_display_contents()) {
+    std::cout << "Display: " << display_container.display_name << std::endl;
+
+    for (const auto &window : display_container.windows) {
+      std::cout << window.pid << std::endl;
+      print_nodes(window.i3node, 1);
     }
   }
 }
 
-void WindowManager::print_nodes(const std::shared_ptr<i3ipc::container_t>& root, int cur_depth) {
+void WindowManager::print_nodes(const std::shared_ptr<i3ipc::container_t> &root, int cur_depth) {
   for (int i = 0; i < cur_depth; i++) {
     std::cout << "\t";
   }
@@ -49,9 +54,63 @@ std::vector<DisplayContent> WindowManager::get_display_contents() {
         continue;
       }
 
-      out.emplace_back(DisplayContent{container->name, node->nodes});
+      std::list<WindowContent> windows;
+      for (const auto &subnode : node->nodes) {
+        windows.emplace_back(this->get_window(subnode));
+      }
+
+      out.emplace_back(DisplayContent{container->name, windows});
     }
   }
 
   return out;
+}
+
+pid_t WindowManager::get_pid(const std::shared_ptr<i3ipc::container_t> &container) {
+  if (!container->xwindow_id) {
+    return 0;
+  }
+
+  if (!this->x_display) {
+    this->x_display = XOpenDisplay(nullptr);
+    assert(this->x_display);
+    this->x_screen = XDefaultScreenOfDisplay(this->x_display);
+    assert(this->x_screen);
+    this->x_pid_atom = XInternAtom(this->x_display, "__NET_WM_PID", true);
+    assert(this->x_pid_atom);
+  }
+
+  Atom actual_type;
+  int actual_format;
+  unsigned long nitems, bytes_after, nbytes;
+  unsigned char *prop;
+  if (XGetWindowProperty(this->x_display,
+                         container->xwindow_id,
+                         this->x_pid_atom,
+                         0,
+                         sizeof(pid_t),
+                         false,
+                         AnyPropertyType,
+                         &actual_type,
+                         &actual_format,
+                         &nitems,
+                         &bytes_after,
+                         &prop) != Success) {
+    return 0;
+  }
+  if (!actual_format) {
+    return 0;
+  }
+
+  return static_cast<pid_t>(atoi(reinterpret_cast<char *>(prop)));
+}
+
+WindowContent WindowManager::get_window(const std::shared_ptr<i3ipc::container_t> &container) {
+  std::list<WindowContent> children;
+
+  for (const auto &node : container->nodes) {
+    children.emplace_back(this->get_window(node));
+  }
+
+  return {this->get_pid(container), children, container};
 }
